80681ab935c44b9d29b1a68297c3d4dfb85f209f
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2024-03-14 14:22:23 +0530

    Hygraph Setup

---
+++ App/Screens/HomeScreen/Slider.jsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React,{useEffect,useState} from 'react'
+import GlobalApi from '../Utils/GlobalApi.js'
+
+export default function Slider() {
+
+    const [slider,setSlider]=useState();
+    useEffect(() => {
+        getSliders();
+      
+    }, [])
+    
+    const getSliders=()=>{
+        GlobalApi.getSlider().then(resp=>{
+            console.log("resp",resp.sliders);
+            setSlider(resp?.sliders)
+        })
+
+    }
+  return (
+    <View>
+      <Text>Slider</Text>
+    </View>
+  )
+}
---
+++ App/Screens/Utils/GlobalApi.js
@@ -0,0 +1,26 @@
+import { request, gql } from 'graphql-request'
+
+
+const MASTER_URL="https://api-ap-south-1.hygraph.com/v2/cltqpw9nk0gcw07th30bk1uxb/master"
+
+    
+    const getSlider = async () => {
+        const query = gql`
+            query GetSlider {
+                sliders {
+                    id
+                    name
+                    image {
+                        url
+                    }
+                }
+            }
+        `;
+const result=await request(MASTER_URL, query);
+return result;
+
+}
+
+export default{
+    getSlider
+}
